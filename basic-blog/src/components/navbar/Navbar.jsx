c897385ae178cc96433906b6b55036294516871a
Becki Wordsworth <becki@hiotlabs.com>
2024-01-08 23:55:57 +0100

    making login work and more routes

--- basic-blog/src/components/navbar/Navbar.jsx
+++ basic-blog/src/components/navbar/Navbar.jsx
@@ -5,13 +5,15 @@ import Image from "next/image";
 import classes from "./navbar.module.css";
 import person from "../../../public/person.jpg";
 import { AiOutlineClose } from "react-icons/ai";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const { data: session } = useSession();
 
   const handleShowDropdown = () => setShowDropdown((prev) => true);
   const handleHideDropdown = () => setShowDropdown((prev) => false);
-  const loggedIn = true;
+  const loggedIn = false;
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
@@ -19,16 +21,27 @@ const Navbar = () => {
           <Link href="/">Becki Wordsworth</Link>
         </h2>
         <ul className={classes.right}>
-          {loggedIn ? (
+          {session?.user ? (
             <div>
               <Image onClick={handleShowDropdown} src={person} width="45" height="45" />
               {showDropdown && (
                 <div className={classes.dropdown}>
                   <AiOutlineClose className={classes.closeIcon} onClick={handleHideDropdown} />
-                  <button onClick={handleHideDropdown} className={classes.logout}>
+                  <button
+                    onClick={() => {
+                      signOut();
+                    }}
+                    className={classes.logout}
+                  >
                     Logout
                   </button>
-                  <Link href="/create-post" onClick={handleHideDropdown} className={classes.create}>
+                  <Link
+                    href="/create-post"
+                    onClick={() => {
+                      signIn();
+                    }}
+                    className={classes.create}
+                  >
                     Create
                   </Link>
                 </div>
@@ -36,7 +49,9 @@ const Navbar = () => {
             </div>
           ) : (
             <>
-              <button className={classes.login}>Login</button>
+              <Link href="/login" className={classes.login}>
+                Login
+              </Link>
               <Link href="/register">Register</Link>
             </>
           )}
