5d98781867f4179854a361e374faabe4c4a45f9f
Becki Wordsworth <becki@hiotlabs.com>
2024-01-12 00:09:51 +0100

    Edit blog page updates

--- basic-blog/src/app/blog/edit/[id]/page.jsx
+++ basic-blog/src/app/blog/edit/[id]/page.jsx
@@ -4,14 +4,40 @@ import React from "react";
 import classes from "./edit.module.css";
 import { useSession } from "next-auth/session";
 
-<navigation></navigation>;
 const Edit = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [category, setCategory] = useState("Nature");
   const [photo, setPhoto] = ueState("");
   const { data: session, status } = useSession();
-  return <div>Edit</div>;
+
+  if (status === "loading") {
+    return <p>Loading ....</p>;
+  }
+
+  if (status === "unauthenticated") {
+    return <p className={classes.accessDenied}>Access Denied</p>;
+  }
+
+  useEffect(() => {
+    async function fetchBlog() {
+      const res = await fetch(`http://localhost:3000/api/blog/${ctx.params.id}`);
+      const blog = await res.json();
+
+      setTitle(blog.title);
+      setDesc(blog.desc);
+      setTitle(blog.category);
+    }
+    fetchBlog();
+  }, []);
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}></div>
+      <h2>Edit Post</h2>
+      <form></form>
+    </div>
+  );
 };
 
 export default Edit;
