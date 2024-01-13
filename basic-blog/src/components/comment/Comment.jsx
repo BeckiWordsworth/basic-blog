49ef0cb452d02bafbe876e16119393b4db0b1790
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-01-13 18:37:41 +0000

    Beginning of add comment

--- basic-blog/src/components/comment/Comment.jsx
+++ basic-blog/src/components/comment/Comment.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import classes from './comment.module.css'
+import React from "react";
+import classes from "./comment.module.css";
+import { useSession } from "next-auth/react";
+import { format } from "timeago.js";
 
-const Comment = () => {
+const Comment = (comment, setComments) => {
+  const { data: session } = useSession();
+  const token = session?.user?.accessToken;
+
+  const handleDeleteComment = async() => {
+    try {
+
+    }catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>Comment</div>
-  )
-}
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Image src={person} width="45" height="45" alt="" />
+          <div className={classes.userData}>
+            <h4>{comment?.authorId?.username}</h4>
+            <span className={classes.timeago}>{format(comment?.createAt)}</span>
+          </div>
+          <span>{comment?.text}</span>
+        </div>
+        <div className={classes.right}>
+          {session?.user?._id === comment?.authorId?._id && (
+            <BsTrash className={classes.trashIcon} onClick={handleDeleteComment}
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Comment
+export default Comment;
